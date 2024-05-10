08821c65db6d319ad1535a9b461db8b316ebf6a9
Rafi <rafi@Rafis-MacBook-Pro.local>
2024-05-10 18:02:18 -0400

    create initial component for introductions

---
+++ components/Introduction.tsx
@@ -0,0 +1,10 @@
+
+export const Introduction = () => {
+    return (
+        <div>
+            <h1>Rafiluis De Los Santos</h1>
+            <h3>Software Engineer</h3>
+            <p>I build digital experiences for web and mobile</p>
+        </div>
+    )
+}
