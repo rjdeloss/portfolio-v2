886171132d97532e9955ab966233b25c16082f05
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-10 19:35:34 -0400

    create navigation anchors

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,10 +1,16 @@
+import { Socials } from "./Socials"
+import { PageNavigation } from "./PageNavigation";
 
 export const Introduction = () => {
     return (
-        <div>
-            <h1>Rafiluis De Los Santos</h1>
-            <h3>Software Engineer</h3>
-            <p>I build digital experiences for web and mobile</p>
-        </div>
+        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+            <div>
+                <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Rafiluis De Los Santos</h1>
+                <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Software Engineer</h2>
+                <p className="mt-4 max-w-xs leading-normal">I build digital experiences for web and mobile</p>
+            </div>
+            <PageNavigation />
+            <Socials />
+        </header>
     )
 }
---
+++ components/PageNavigation.tsx
@@ -0,0 +1,17 @@
+const links = ["About", "Experience", "Projects"]
+export const PageNavigation = () => {
+  return (
+    <nav className="nav hidden lg:block" aria-label="In-page jump links">
+      <ul className="mt-16 w-max">
+        {links.map(link => (
+          <li key={link}>
+            <a className="group flex items-center py-3 active" href={`#${link.toLocaleLowerCase()}`}>
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">{link}</span>
+            </a>
+        </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
