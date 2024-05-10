a7e7a3df7aa7188dafcbff54a376b31540213d01
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-10 19:52:32 -0400

    create About component

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -7,7 +7,7 @@ export const Introduction = () => {
             <div>
                 <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Rafiluis De Los Santos</h1>
                 <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Software Engineer</h2>
-                <p className="mt-4 max-w-xs leading-normal">I build digital experiences for web and mobile</p>
+                <p className="mt-4 max-w-xs leading-normal">I build digital experiences for the web and mobile</p>
             </div>
             <PageNavigation />
             <Socials />
