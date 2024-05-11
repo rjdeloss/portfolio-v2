d6a9a06da07f0d4ac79f31a60db279466b9e431a
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-10 22:14:35 -0400

    wrap up adding experience skeleton

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,3 +1,4 @@
+// this needs to be broken down further. An object needs to be passed as work history.
 const JobDescription = () => {
   return (
     <li className="mb-12">
@@ -51,6 +52,21 @@ export const Experience = () => {
         <ol className="group/list">
           <JobDescription />
         </ol>
+        <div className="mt-12">
+          <a href="/resume.pdf"
+            className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="View Full Résumé (opens in a new tab)"
+          >
+            {/* add link to your actual resume */}
+            <span>View Full Resume
+              <span className="inline-block"> 
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
+              </span>
+            </span>
+          </a>
+        </div>
       </div>
 
     </section>
