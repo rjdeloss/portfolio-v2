077aa31f2e8feee504487e09ec33ce4ca43842aa
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-10 20:39:59 -0400

    skeleton for experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -21,6 +21,20 @@ const JobDescription = () => {
               </div>
             </h3>
             <p className="mt-2 text-sm leading-normal">Description of the work experience goes here</p>
+            <ul className="mt-2 flex flex-wrap" aria-label="technilogies used">
+              <li className="mr-1.5 mt-2">
+                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 "> JavaScript</div>
+              </li>
+              <li className="mr-1.5 mt-2">
+                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 "> JavaScript</div>
+              </li>
+              <li className="mr-1.5 mt-2">
+                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 "> JavaScript</div>
+              </li>
+              <li className="mr-1.5 mt-2">
+                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 "> JavaScript</div>
+              </li>
+            </ul>
           </div>
       </div>
     </li>
