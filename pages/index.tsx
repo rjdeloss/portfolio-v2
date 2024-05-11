5b93ce22bd849123cf6c505ea3c3c89ffaffe894
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-11 16:28:23 -0400

    normalize experience data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import beach from "@/images/beach.jpg"
 import { Inter } from "next/font/google";
 import { Introduction } from "@/components/Introduction";
 import { About } from "@/components/About";
@@ -10,16 +10,18 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-      <main className="lg:flex lg:justify-between lg:gap-4">
-        <Introduction />
-        <div id="content" className="pt-24 lg:w-1/2 lg:py-24">
-          <About />
-          <Experience />
-          <Projects />
-          <Footer />
-        </div>
-      </main>
-    </div>
+    // <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url(${beach.src})`, width: '100%'}}>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <main className="lg:flex lg:justify-between lg:gap-4">
+          <Introduction />
+          <div id="content" className="pt-24 lg:w-1/2 lg:py-24">
+            <About />
+            <Experience />
+            <Projects />
+            <Footer />
+          </div>
+        </main>
+      </div>
+    // </div>
   );
 }
