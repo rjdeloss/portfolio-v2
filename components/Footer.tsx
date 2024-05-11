723de69b9ac28db74dfa9bee036576b35a14026f
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-11 09:49:36 -0400

    wrap up footer skeleton

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+      <p>Inspired by <a href="https://brittanychiang.com/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" aria-label="Brittany Chiang's portfolio">Brittany Chiang's portfolio</a> and coded in <a href="https://code.visualstudio.com/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer noopener" aria-label="Visual Studio Code (opens in a new tab)">Visual Studio Code</a> by my hands. Built with <a href="https://nextjs.org/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer noopener" aria-label="Next.js (opens in a new tab)">Next.js</a> and <a href="https://tailwindcss.com/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer noopener" aria-label="Tailwind CSS (opens in a new tab)">Tailwind CSS</a>. 
+      </p>
+    </footer>
+  )
+}
