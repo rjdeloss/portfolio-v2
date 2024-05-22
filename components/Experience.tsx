78c665667d2f7dc7c5565f42a175fed56bebb701
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-05-22 00:30:22 -0400

    adjust columns for projects and experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,10 +8,10 @@ const jobDescription = (experience:any, i:number) => {
 
   return (
     <li className="mb-12">
-      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-40">
+      <div className="group relative grid pb-1 transition-all sm:grid-cols-10 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-40">
         <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-          <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" arial-label="date">{date}</header>
-          <div className="z-10 sm:col-span-6">
+          <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-3" arial-label="date">{date}</header>
+          <div className="z-10 sm:col-span-7">
             <h3 className="font-medium leading-snug text-slate-200">
               <div>
                 <a href={website}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,9 +7,9 @@ const projectFormat = (project:any) => {
   console.log(technologiesUsed)
   return (
     <li className="mb-12">
-      <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+      <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-10 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
         <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-        <div className="z-10 sm:order-2 sm:col-span-6">
+        <div className="z-10 sm:order-2 sm:col-span-7">
           <h3>
             {/* make sure to add the correct aria-label when creating the object */}
             <a href={website}
@@ -40,7 +40,7 @@ const projectFormat = (project:any) => {
           alt="beach bg"
           width={200}
           height={100}
-          className="z-10 col-span-2 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2"
+          className="z-10 col-span-3 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-3"
         />
       </div>
     </li>
